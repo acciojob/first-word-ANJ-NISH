d942393eb18debec05a86d15d43e575e45d1d71c
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-29 11:20:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function firstWord(s) {
-  // your code here
+
+	let len=s.length;
+	let i=0;
+
+	while(i<len && s.charAt(i)===" ")
+		{
+			i++;
+		}
+
+	let ans="";
+
+	while(i<len && s.charAt(i)!==" ")
+		{
+			ans+=s.charAt(i);
+			i++;
+		}
+
+	return ans;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
